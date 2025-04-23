1b352495df9ef153d2f47fede4fd797374fa1410
Mikołaj_Puchała <mikolaj.puchala1@student.up.krakow.pl>
2025-04-23 21:09:35 +0200

    Update CruddemoApplicationTests.java

--- ProjektMP/src/test/java/com/MP/Homework/CruddemoApplicationTests.java
+++ ProjektMP/src/test/java/com/MP/Homework/CruddemoApplicationTests.java
@@ -2,8 +2,10 @@ package com.MP.Homework;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")	
 class CruddemoApplicationTests {
 
 	@Test
